809d1abb17e15060ca08596f1f870ae11607d8ed
Honore-Pacifique <honore.bayisenge@bosa.fgov.be>
2025-02-04 16:41:55 +0100

    vercel api formatting

--- api/index.js
+++ api/index.js
@@ -1,38 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const PUSH_MANAGER = require("../pushManager");
+const PUSH_MANAGER = require("./pushManager");
 
 const app = express();
 app.use(express.json());
-
-// Enable CORS
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: "*",
-  })
-);
+app.use(cors({ origin: "*", methods: ["GET", "POST"], allowedHeaders: "*" }));
 
 // API Endpoints
-app.get("/api/test", (req, res) => {
-  return res.status(200).json({
-    message: "Already registered for push notifications",
-  });
-});
+app.get("/api/test", (req, res) => {  return res.status(200).json({    message: "testing",  });});
 
+app.post("/api/subscribe", (req, res) => PUSH_MANAGER.subscribePush(req, res));
+app.post("/api/unsubscribe", (req, res) => PUSH_MANAGER.unsubscribePush(req, res));
+app.post("/api/send-notification", async (req, res) => await PUSH_MANAGER.sendPush(req, res));
 
-app.post("/api/subscribe", (req, res) => {
-  PUSH_MANAGER.subscribePush(req, res);
-});
-
-app.post("/api/unsubscribe", (req, res) => {
-  PUSH_MANAGER.unsubscribePush(req, res);
-});
-
-app.post("/api/send-notification", async (req, res) => {
-  await PUSH_MANAGER.sendPush(req, res);
-});
-
-// Export the handler for Vercel
-module.exports = app
+module.exports = app;
